a09323554d6d4ad763ff38198bd2cbe22385df4d
Alexander de Jong <mrdejong89@gmail.com>
2021-02-03 15:21:30 +0100

    Initial commit
    Already configured the whole shell!

--- config.fish
+++ config.fish
@@ -1,18 +1,21 @@
-set -U fish_user_paths $HOME/.local/bin $HOME/Applications $fish_user_paths
+set -U fish_user_paths $HOME/.cargo/bin $HOME/.local/bin $HOME/Applications $fish_user_paths
 set fish_greeting
 set TERM "xterm-256color"
 set EDITOR "nvim"
 
-function fish_user_key_bindings
-  fish_vi_key_bindings
-end
+set FZF_DEFAULT_COMMAND "ag -p ~/.gitignore -g \"\""
 
 alias ..='cd ..'
 alias ...='cd ../..'
+alias vc="nvim $HOME/.config/nvim"
+
+fish_default_key_bindings
 
 if status --is-interactive
   set BASE16_SHELL "$HOME/.config/base16-shell/"
   source "$BASE16_SHELL/profile_helper.fish"
 end
+
+source ~/.asdf/asdf.fish
 
 starship init fish | source
